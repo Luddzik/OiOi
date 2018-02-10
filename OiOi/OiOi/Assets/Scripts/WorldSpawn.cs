361d986071ea641691f73c9fd36e6e02ce7a7d1b
Ludie <luddzik@gmail.com>
2018-02-10 15:22:02 +0000

    Created Prototype to test world tracking. Also updated the Developer Diary accordingly

---
+++ OiOi/OiOi/Assets/Scripts/WorldSpawn.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace UnityEngine.XR.iOS
+{
+	public class WorldSpawn: MonoBehaviour
+	{
+		[SerializeField] private GameObject planet;
+		
+		public Transform m_HitTransform;
+		public float maxRayDistance = 30.0f;
+		public LayerMask collisionLayer = 1 << 10;  //ARKitPlane layer
+
+		private bool worldSpawn = false;
+
+		bool HitTestWithResultType (ARPoint point, ARHitTestResultType resultTypes)
+		{
+			List<ARHitTestResult> hitResults = UnityARSessionNativeInterface.GetARSessionNativeInterface ().HitTest (point, resultTypes);
+			if (hitResults.Count > 0) {
+				foreach (var hitResult in hitResults) {
+					worldSpawn = true;
+
+					Debug.Log ("Got hit!");
+					m_HitTransform.position = UnityARMatrixOps.GetPosition (hitResult.worldTransform);
+					m_HitTransform.rotation = UnityARMatrixOps.GetRotation (hitResult.worldTransform);
+
+					PlanetsSpawn();
+
+					Debug.Log (string.Format ("x:{0:0.######} y:{1:0.######} z:{2:0.######}", m_HitTransform.position.x, m_HitTransform.position.y, m_HitTransform.position.z));
+					return true;
+				}
+			}
+			return false;
+		}
+
+		void PlanetsSpawn () 
+		{
+			GameObject[] planets = new GameObject[2];
+
+			planets[0] = (GameObject) Instantiate(planet);
+			planets[0].transform.position = new Vector3(m_HitTransform.position.x, m_HitTransform.position.y + 1.5f, m_HitTransform.position.z + 1.5f);
+
+			planets[1] = (GameObject) Instantiate(planet);
+			planets[1].transform.position = new Vector3(m_HitTransform.position.x, m_HitTransform.position.y + 1.5f, m_HitTransform.position.z - 1.5f);
+		}
+
+		// Update is called once per frame
+		void Update () 
+		{
+			if ((Input.touchCount > 0 && m_HitTransform != null) && !worldSpawn)
+			{
+				var touch = Input.GetTouch(0);
+				if (touch.phase == TouchPhase.Began || touch.phase == TouchPhase.Moved)
+				{
+					var screenPosition = Camera.main.ScreenToViewportPoint(touch.position);
+					ARPoint point = new ARPoint {
+						x = screenPosition.x,
+						y = screenPosition.y
+					};
+
+					// prioritize reults types
+					ARHitTestResultType[] resultTypes = {
+						ARHitTestResultType.ARHitTestResultTypeExistingPlaneUsingExtent, 
+						// if you want to use infinite planes use this:
+						//ARHitTestResultType.ARHitTestResultTypeExistingPlane,
+						ARHitTestResultType.ARHitTestResultTypeHorizontalPlane, 
+						ARHitTestResultType.ARHitTestResultTypeFeaturePoint
+					}; 
+
+					foreach (ARHitTestResultType resultType in resultTypes)
+					{
+						if (HitTestWithResultType (point, resultType))
+						{
+							return;
+						}
+					}
+				}
+			}
+
+		}
+
+
+	}
+}
+
