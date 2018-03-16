7f8a8f27c4f44902b1b026c85d26f8aee9ff64c4
Ludie <luddzik@gmail.com>
2018-03-16 22:17:33 +0000

    Updated Developer Diary. Due to feedback, the new implementation is being done for the game. The introductory transition of planets and assets is being completed.

---
+++ OiOi/OiOi/Assets/Scripts/GameManager.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UnityEngine.XR.iOS
+{
+    public class GameManager : MonoBehaviour
+    {
+        [SerializeField] private GameObject spawn;
+
+        IEnumerator GameSequence()
+        {
+            Transform spawnLoc = spawn.GetComponent<Spawn>().m_HitTransform;
+
+            Vector3 planetLoc = spawn.GetComponent<Spawn>().m_HitTransform.position;
+            Vector3 planetEndPos = spawn.GetComponent<Spawn>().GetPlanetTransform(0);
+
+            float planetScale = spawn.GetComponent<Spawn>().GetPlanetScale(0);
+
+            float newPlanetScale = planetScale / 10.0f;
+
+            float temp = newPlanetScale;
+
+            spawn.GetComponent<Spawn>().SetPlanetScale(0, new Vector3(newPlanetScale, newPlanetScale, newPlanetScale));
+
+            spawn.GetComponent<Spawn>().SetPlanetPosition(0, planetLoc);
+
+            spawn.GetComponent<Spawn>().SetPlanetStatus(0, true);
+
+            Vector3 x = (planetLoc - planetEndPos);
+            float s = x.magnitude;
+            //spawn.GetComponent<Spawn>().SetPlanetPosition(0, planetEndPos);
+
+
+            while (Vector3.Distance(planetLoc, planetEndPos) > 0.01f)
+            {
+                spawn.GetComponent<Spawn>().SetPlanetPosition(0, Vector3.Lerp(planetLoc, planetEndPos, s * Time.deltaTime));
+
+                if (newPlanetScale < planetScale)
+                {
+                    newPlanetScale += planetScale * Time.deltaTime;
+                    spawn.GetComponent<Spawn>().SetPlanetScale(0, new Vector3(newPlanetScale, newPlanetScale, newPlanetScale));
+                }
+                else if (newPlanetScale > planetScale)
+                {
+                    newPlanetScale = planetScale;
+                    spawn.GetComponent<Spawn>().SetPlanetScale(0, new Vector3(newPlanetScale, newPlanetScale, newPlanetScale));
+                }
+
+                planetLoc = spawn.GetComponent<Spawn>().GetPlanetTransform(0);
+
+                yield return new WaitForSeconds(0.1f);
+            }
+
+            //yield return new WaitForSeconds(0.1f);
+        }
+
+    }
+}
---
+++ OiOi/OiOi/Assets/Scripts/Spawn.cs
@@ -0,0 +1,190 @@
+﻿//using System;
+using System.Collections;
+using System.Collections.Generic;
+//using UnityEditor.UI;
+
+namespace UnityEngine.XR.iOS
+{
+    public class Spawn : MonoBehaviour
+    {
+        [SerializeField] private GameObject[] planetPrefab;
+        [SerializeField] private Material[] planetMaterial;
+
+        [SerializeField] private GameObject gameManager;
+
+        private GameObject[] planet;
+        private bool worldSpawn = false;
+
+        public Transform m_HitTransform;
+        public float maxRayDistance = 30.0f;
+        public LayerMask collisionLayer = 1 << 10;  //ARKitPlane layer
+
+        public void SetPlanetStatus (int i, bool status)
+        {
+            planet[i].SetActive(status);
+        }
+
+        public Vector3 GetPlanetTransform (int i)
+        {
+            return planet[i].transform.position;
+        }
+
+        public void SetPlanetPosition (int i, Vector3 pos)
+        {
+            planet[i].transform.position = new Vector3(pos.x, pos.y, pos.z);
+        }
+
+        public float GetPlanetScale (int i)
+        {
+            return planet[i].transform.localScale.x;
+        }
+
+        public void SetPlanetScale (int i, Vector3 growth)
+        {
+            planet[i].transform.localScale = growth;
+        }
+
+        bool HitTestWithResultType(ARPoint point, ARHitTestResultType resultTypes)
+        {
+            List<ARHitTestResult> hitResults = UnityARSessionNativeInterface.GetARSessionNativeInterface().HitTest(point, resultTypes);
+            if (hitResults.Count > 0 && !worldSpawn)
+            {
+                foreach (var hitResult in hitResults)
+                {
+                    worldSpawn = true;
+
+                    //Debug.Log ("Got hit!");
+                    m_HitTransform.position = UnityARMatrixOps.GetPosition(hitResult.worldTransform);
+                    m_HitTransform.rotation = UnityARMatrixOps.GetRotation(hitResult.worldTransform);
+
+                    PlanetsSpawn();
+
+                    gameManager.GetComponent<GameManager>().StartCoroutine("GameSequence");
+
+                    //Debug.Log (string.Format ("x:{0:0.######} y:{1:0.######} z:{2:0.######}", m_HitTransform.position.x, m_HitTransform.position.y, m_HitTransform.position.z));
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        void PlanetsSpawn()
+        {
+            int objectNumber = 10;
+            planet = new GameObject[objectNumber];
+
+            int x;
+
+            x = Random.Range(0, planetPrefab.Length);
+
+            planet[0] = (GameObject)Instantiate(planetPrefab[x]);
+            planet[0].transform.position = new Vector3(m_HitTransform.position.x, m_HitTransform.position.y + 1.5f, m_HitTransform.position.z + 0.1f);
+            planet[0].GetComponent<Renderer>().material = new Material(planetMaterial[0]);
+            planet[0].SetActive(false);
+
+            x = Random.Range(0, planetPrefab.Length);
+
+            planet[1] = (GameObject)Instantiate(planetPrefab[x]);
+            planet[1].transform.position = new Vector3(m_HitTransform.position.x + 1.0f, m_HitTransform.position.y + 1.2f, m_HitTransform.position.z);
+            planet[1].GetComponent<Renderer>().material = new Material(planetMaterial[0]);
+            planet[1].SetActive(false);
+
+            x = Random.Range(0, planetPrefab.Length);
+
+            planet[2] = (GameObject)Instantiate(planetPrefab[x]);
+            planet[2].transform.position = new Vector3(m_HitTransform.position.x + 0.5f, m_HitTransform.position.y + 1f, m_HitTransform.position.z + 0.5f);
+            planet[2].GetComponent<Renderer>().material = new Material(planetMaterial[0]);
+            planet[2].SetActive(false);
+
+            x = Random.Range(0, planetPrefab.Length);
+
+            planet[3] = (GameObject)Instantiate(planetPrefab[x]);
+            planet[3].transform.position = new Vector3(m_HitTransform.position.x, m_HitTransform.position.y + 0.9f, m_HitTransform.position.z + 1.0f);
+            planet[3].GetComponent<Renderer>().material = new Material(planetMaterial[0]);
+            planet[3].SetActive(false);
+
+            x = Random.Range(0, planetPrefab.Length);
+
+            planet[4] = (GameObject)Instantiate(planetPrefab[x]);
+            planet[4].transform.position = new Vector3(m_HitTransform.position.x - 0.5f, m_HitTransform.position.y + 1.2f, m_HitTransform.position.z - 0.5f);
+            planet[4].GetComponent<Renderer>().material = new Material(planetMaterial[0]);
+            planet[4].SetActive(false);
+
+            x = Random.Range(0, planetPrefab.Length);
+
+            planet[5] = (GameObject)Instantiate(planetPrefab[x]);
+            planet[5].transform.position = new Vector3(m_HitTransform.position.x - 1.0f, m_HitTransform.position.y + 1.5f, m_HitTransform.position.z);
+            planet[5].GetComponent<Renderer>().material = new Material(planetMaterial[0]);
+            planet[5].SetActive(false);
+
+            x = Random.Range(0, planetPrefab.Length);
+
+            planet[6] = (GameObject)Instantiate(planetPrefab[x]);
+            planet[6].transform.position = new Vector3(m_HitTransform.position.x - 0.5f, m_HitTransform.position.y + 1.2f, m_HitTransform.position.z + 0.5f);
+            planet[6].GetComponent<Renderer>().material = new Material(planetMaterial[0]);
+            planet[6].SetActive(false);
+
+            x = Random.Range(0, planetPrefab.Length);
+
+            planet[7] = (GameObject)Instantiate(planetPrefab[x]);
+            planet[7].transform.position = new Vector3(m_HitTransform.position.x + 0.5f, m_HitTransform.position.y + 1f, m_HitTransform.position.z - 0.5f);
+            planet[7].GetComponent<Renderer>().material = new Material(planetMaterial[0]);
+            planet[7].SetActive(false);
+
+            x = Random.Range(0, planetPrefab.Length);
+
+            planet[8] = (GameObject)Instantiate(planetPrefab[x]);
+            planet[8].transform.position = new Vector3(m_HitTransform.position.x, m_HitTransform.position.y + 0.9f, m_HitTransform.position.z - 1.0f);
+            planet[8].GetComponent<Renderer>().material = new Material(planetMaterial[0]);
+            planet[8].SetActive(false);
+
+            x = Random.Range(0, planetPrefab.Length);
+
+            planet[9] = (GameObject)Instantiate(planetPrefab[x]);
+            planet[9].transform.position = new Vector3(m_HitTransform.position.x + 2.0f, m_HitTransform.position.y + 0.5f, m_HitTransform.position.z);
+            planet[9].GetComponent<Renderer>().material = new Material(planetMaterial[0]);
+            planet[9].SetActive(false);
+        }
+
+        void Update()
+        {
+            if ((Input.touchCount > 0 && m_HitTransform != null) /*&& !worldSpawn*/)
+            {
+                var touch = Input.GetTouch(0);
+                if (touch.phase == TouchPhase.Began || touch.phase == TouchPhase.Moved)
+                {
+                    var screenPosition = Camera.main.ScreenToViewportPoint(touch.position);
+
+                    Ray ray = Camera.main.ScreenPointToRay(touch.position);
+
+                    ARPoint point = new ARPoint
+                    {
+                        x = screenPosition.x,
+                        y = screenPosition.y
+                    };
+
+                    // prioritize reults types
+                    ARHitTestResultType[] resultTypes = {
+                        ARHitTestResultType.ARHitTestResultTypeExistingPlaneUsingExtent, 
+                        // if you want to use infinite planes use this:
+                        ARHitTestResultType.ARHitTestResultTypeExistingPlane,
+                        ARHitTestResultType.ARHitTestResultTypeHorizontalPlane, 
+                        ARHitTestResultType.ARHitTestResultTypeFeaturePoint
+                    };
+
+                    foreach (ARHitTestResultType resultType in resultTypes)
+                    {
+                        if (HitTestWithResultType(point, resultType))
+                        {
+                            return;
+                        }
+                    }
+
+
+                }
+            }
+
+        }
+    }
+}
